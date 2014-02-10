3f9ca91b51412c4b2107320579acdf30951dfcf8
Joe Murphy <joe.murphy@gmail.com>
2014-02-09 22:11:05 -0700

    Added beginner's setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+from distutils.core import setup
+
+setup(
+    name='FancyText',
+    version='0.2',
+    description='Fancy up that terminal text.',
+    license='Apache',
+    url='https://github.com/freejoe76/fancytext',
+    author='Joe Murphy',
+    author_email='joe.murphy@gmail.com',
+    packages=['fancytext'],
+    )
