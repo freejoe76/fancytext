8418cdb41da581a087d5a57fa4ac2212c4f686ff
Joe Murphy <joe.murphy@gmail.com>
2014-02-11 00:20:41 -0700

    Added numbers test.

--- tests/test_fancytext.py
+++ tests/test_fancytext.py
@@ -3,11 +3,11 @@
 from __future__ import unicode_literals
 import pytest
 from fancytext.fancytext import FancyText
-#import fancytext
 
-def func(x):
-    return x + 1
-
-def test_fancytext():
+def test_letters():
     u = FancyText('monospace')
     assert u.translate('TEST') == '𝚃𝙴𝚂𝚃'
+
+def test_numbers():
+    u = FancyText('monospace')
+    assert u.translate('12345') == '𝟷𝟸𝟹𝟺𝟻'
